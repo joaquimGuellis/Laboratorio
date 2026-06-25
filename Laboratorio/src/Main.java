805b97bff2177e67db1f0400be6234109358e079
Joaquim <joaquimguellisdev@gmail.com>
2026-06-25 09:52:39 -0300

    Conclusão For
    Conclusão do laço For

--- Laboratorio/src/Main.java
+++ Laboratorio/src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
   public static void main(String[] args) {
     for (int i = 1; i <= 10; i++) {
-      System.out.println(i);
+      System.out.println("Numero: " + i);
     }
+    /*For é usado quando você sabe quantas vezes quer repetir algo, como no exemplo acima */
   }
 }
