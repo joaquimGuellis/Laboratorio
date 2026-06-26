c0dbf289cedbb1843617b4a1e88c03780951963e
Joaquim <joaquimguellisdev@gmail.com>
2026-06-26 00:46:01 -0300

    estrutura Do While
    Estrutura do do while

--- Laboratorio/src/Main.java
+++ Laboratorio/src/Main.java
@@ -2,12 +2,11 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    System.out.println("Digite a senha");
-    String senha_1 = sc.nextLine();
-    while (!senha_1.equals("java123")) {
-      System.out.println("Digite a senha");
-      senha_1 = sc.nextLine();
-    }
-    System.out.println("acesso liberado");
+    int i = 1;
+
+    do {
+      System.out.println(i);
+      i++;
+    } while (i <= 10);
   }
 }
